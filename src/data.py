f971fbc74d3fa2b38ee66a2bda7e5ee3077cad5d
Sukhija-Aniket <b20081@students.iitmandi.ac.in>
2024-04-24 03:46:52 +0530

    fixed minor issue

--- src/data.py
+++ src/data.py
@@ -43,7 +43,29 @@ class Histology(Dataset):
                 augmentations = self.transform(image=image)
                 image = augmentations['image']
                 return image, None
+            
+class Histology_colorization(Dataset):
+    def __init__(self, image_dir, transform=None):
+        self.image_dir = image_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
 
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        image = np.array(Image.open(img_path).convert('RGB'))
+        image_grayscale =  np.array(Image.open(img_path).convert('L'), dtype=np.float32)
+        image_grayscale[image_grayscale== 255.0] = 1.0
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, image_grayscale = image_grayscale)
+            image = augmentations['image']
+            image_grayscale = augmentations['image_grayscale']
+
+        return image_grayscale, image
+    
 class ContrastiveRotationDataset(Dataset):
     def __init__(self, directory, transform=None):
         """
