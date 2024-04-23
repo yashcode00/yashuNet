02ee3edc2a87abdf88761f4aebef83fb090834f4
Yash Sharma <yashuvats.42@gmail.com>
2024-04-23 19:45:19 +0530

    minor change

--- scripts/run_train_colorization.sh
+++ scripts/run_train_colorization.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #SBATCH -N 1
 #SBATCH --partition=nltmp
-#SBATCH --job-name=yamaha
-#SBATCH --gres=gpu:1
+#SBATCH --job-name=colr-yamaha
+#SBATCH --gres=gpu:2
 #SBATCH --output=/nlsasfs/home/nltm-st/sujitk/temp/yashuNet/logs/out.log  # Updated output path
 #SBATCH --error=/nlsasfs/home/nltm-st/sujitk/temp/yashuNet/logs/err.log    # Updated error path
 #SBATCH --time=7-0:0:0  # 7 days, 0 hours, 0 minutes, and 0 seconds (you can adjust this as needed)
 
 # Define the Conda environment, activate it, and define the Python script and log file
 log_dir="/nlsasfs/home/nltm-st/sujitk/temp/yashuNet/logs/"
-output_main="${log_dir}train-all.log"
+output_main="${log_dir}train-color.log"
 
 eval "$(conda shell.bash hook)" &> /nlsasfs/home/nltm-st/sujitk/temp/yashuNet/logs/error.txt
 
@@ -24,7 +24,7 @@ export ALL_PROXY='http://proxy-10g.10g.siddhi.param:9090'
 
 # Run Python script in the background and save the output to the log file
 # accelerate launch /nlsasfs/home/nltm-st/sujitk/yash-mtp/src/tdnn/xVectorTraining-ddp.py &> "$output_main" &
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch --nproc_per_node=1 /nlsasfs/home/nltm-st/sujitk/temp/yashuNet/src/train.py &> "$output_main" &
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch --nproc_per_node=2 --master_port 12335 /nlsasfs/home/nltm-st/sujitk/temp/yashuNet/src/train_colorization.py &> "$output_main" &
 
 # python3 /nlsasfs/home/nltm-st/sujitk/temp/eeg2text/src/common/utils/data.py  &> "$output_main" &
 
